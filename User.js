fa37e27aded45a7293f3ea40418e9ee52ca5db72
LoudMouth581 <offbeategg@gmail.com>
2026-02-07 19:50:59 -0800

    Update User.js

--- User.js
+++ User.js
@@ -1,16 +1,1 @@
-// models/User.js
-const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String, // hashed!
-  memberId: String,
-  location: {
-    lat: Number,
-    lng: Number
-  },
-  friends: [String] // array of memberIds
-});
-
-module.exports = mongoose.model('User', userSchema);
